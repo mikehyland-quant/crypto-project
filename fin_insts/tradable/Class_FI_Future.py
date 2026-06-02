3f35fa016c598ac64262e7609a0f428e0c303d63
Michael Hyland <michael.j.hyland.jr@gmail.com>
2026-06-01 21:33:45 -0400

    changes

--- fin_insts/tradable/Class_FI_Future.py
+++ fin_insts/tradable/Class_FI_Future.py
@@ -1,7 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
 
-# In[ ]:
 
 
 from fin_insts.parents.Class_FI import FinancialInstrument
@@ -14,7 +11,6 @@ from zoneinfo import ZoneInfo
 import pandas_market_calendars as mcal
 _NYSE = mcal.get_calendar("NYSE")   # module-level, built once
 
-# In[ ]:
 
 '''
 Adds the following attributes: self.date_expire / self.date_expiry_settle / self.days_expiry_settle / self.last_trade_date_time_nyc
@@ -38,7 +34,16 @@ class Future(FinancialInstrument):
             expiration_date  = Dates.date_from_string(self.ibkr_details.realExpirationDate) 
             last_trade_time  = Dates.time_from_number(self.ibkr_details.lastTradeTime, 24)
             tz_exch          = ZoneInfo(self.ibkr_details.timeZoneId or "US/Central")
-                
+
+        elif self.my_pf_name == "Coinbase-Derivs":
+            row = self.fi_row
+            expiration_dt_utc = datetime.fromisoformat(row.future_product_details_contract_expiry.replace("Z", "+00:00"))
+            tz_exch = ZoneInfo(row.future_product_details_contract_expiry_timezone or "UTC")
+
+            expiration_dt_exch = expiration_dt_utc.astimezone(tz_exch)
+            expiration_date = expiration_dt_exch.date()
+            last_trade_time = expiration_dt_exch.time()
+            
         self.date_expiry              = expiration_date
         date_expiry_pmt               = expiration_date + timedelta(days=self.biz_days_to_expiry_pmt)
         self.date_settle_expiry       = Dates.next_nyse_trading_day(date_expiry_pmt)
