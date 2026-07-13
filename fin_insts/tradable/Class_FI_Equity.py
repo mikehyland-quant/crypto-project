df1061834bd63b722e742a4e2527afd6c46d3834
Michael Hyland <michael.j.hyland.jr@gmail.com>
2026-07-12 21:29:38 -0400

    changes

--- fin_insts/tradable/Class_FI_Equity.py
+++ fin_insts/tradable/Class_FI_Equity.py
@@ -24,19 +24,19 @@ class Equity(FinancialInstrument):
         # overwrite previous entries and reattach scalars
         self.scalar_size_raw_to_screen   = 100
 
-        self.scalar_size_FIs_per_unit    = self.get_scalar()
+        self.scalar_size_FIs_per_unit    = self.get_scalar_crypto()
         self.scalar_size_units_per_FI    = 1 / self.scalar_size_FIs_per_unit
 
         self.scalar_size_orders_per_unit = self.scalar_size_FIs_per_unit / self.scalar_size_FIs_per_order
         self.scalar_size_units_per_order = self.scalar_size_FIs_per_order / self.scalar_size_FIs_per_unit
 
 
-    def get_scalar(self):
+    def get_scalar_crypto(self):
         from input_output.Class_InputOutput import InputOutput
         io = InputOutput()
 
-        wb, ws = io.set_xw_book_and_sheet('2026 BTC ETF Ratios.xlsx', 'BTC RATIOS')
-        df = io.get_xw_df(ws, 'btc_ratios', table=True)
+        wb, ws = io.set_xw_book_and_sheet('2026 BTC ETF Ratios.xlsx', 'SCALARS')
+        df = io.get_xw_df(ws, 'scalars', table=True)
         
         if self.my_fi_name not in df.columns:
             return 1
@@ -46,5 +46,49 @@ class Equity(FinancialInstrument):
         scalar = self._safe_float(df.loc[df['Date'] == exp_date, self.my_fi_name].to_list()[0])
         return scalar
         
- 
+    def get_scalar_stat_arb(self):
+        from input_output.Class_InputOutput import InputOutput
+        io = InputOutput()
+
+        wb, ws = io.set_xw_book_and_sheet('2026 Stat Arb Mkt Data.xlsx', 'SCALARS')
+        df = io.get_xw_df(ws, 'scalars', table=True)
+        df = df.T
+        
+        # df['Date'] = pd.to_datetime(df['Date']).dt.date
+        # exp_date = self.date_settle_trade
+        scalar = self._safe_float(df.loc["ratios", self.my_fi_name].to_list()[0])
+        return scalar
+    
+    def get_scalar(self):
+        from input_output.Class_InputOutput import InputOutput
+        io = InputOutput()
+
+        my_fi_name = self.my_fi_name
+        family = self.family
+
+        wb_dict = {'Crypto'   : '2026 BTC ETF Ratios.xlsx',
+                   "Stat Arb" : '2026 Stat Arb Mkt Data.xlsx'}
+
+        wb_name = wb_dict[family]
+
+        wb, ws = io.set_xw_book_and_sheet(wb_name, 'SCALARS')
+        df = io.get_xw_df(ws, 'scalars', table=True)
+        
+        if my_fi_name not in df.columns:
+            df = df.T
+
+        if family == 'Crypto':
+            df['Date'] = pd.to_datetime(df['Date']).dt.date
+            exp_date = self.date_settle_trade
+            row = df.loc[exp_date]
+
+        elif family == 'Stat Arb':
+            row = df.loc['ratios']
+
+        scalar = self._safe_float(row[my_fi_name])
+
+        return scalar
+        
+
+        
         
